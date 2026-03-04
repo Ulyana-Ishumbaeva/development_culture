0d26b0774f435720ea0071fba73fbd0e2d1d2b85
Ulyana Ishumbaeva <isumbaevau@gmail.com>
2026-03-04 16:22:06 +0800

    Fix add function and update test_principles.py

--- testing/tests/test_principles.py
+++ testing/tests/test_principles.py
@@ -1,11 +1,23 @@
 import sys
-sys.path.append("../src")
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
 #TODO make it with 'pip intall -e .'
 
-from math_demo.py import add
+from math_demo import (add, add_with_bug)
 
 def test_addition():
-    assert add(2,2) == 4
+    assert add(2,2) == 4, "Function did not return 4"
     print("Test BASIC ADDITION")
-if __name__ == "main":
+
+def test_addiction_with_bug():
+    assert add_with_bug(2,2) == 4, "Function did not return 4"
+    assert add_with_bug(0,0) == 0
+    print("Test BUGGED ADDITION PASSED (does in mean code ok?)")
+def test_addition_duplicated():
+    #is it real good test(reties on absence of + int add())
+    assert add(2,3) == 2+3
+if __name__ == "__main__":
     test_addition()
+    test_addiction_with_bug()
+    test_addiction_duplicated()
